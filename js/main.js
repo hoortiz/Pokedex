ca5796580bc43681f267c56fd30c7dd9f09d717c
Henrry Ortiz <hoortiz@gmail.com>
2023-07-05 18:07:29 -0500

    Agregar funcion de busqueda

--- js/main.js
+++ js/main.js
@@ -2,16 +2,26 @@ let idpokemonapintar = 4;
 async function pintarpokemon(){
 let responsoinfopoquemon = await obtenerinfopoquemon(idpokemonapintar);
 let contenedorpoquemon = document.querySelector('#contenedorpokemon');
+
+/* Extraer Informacion de la respuesta del API del JSON */
 let urlimagenpoquemon = responsoinfopoquemon["sprites"]["other"]["official-artwork"]["front_default"];
 let nombrepoquemon = responsoinfopoquemon["name"];
 let alturapoquemon = responsoinfopoquemon["height"];
 let pesopoquemon = responsoinfopoquemon["weight"];
 let tipospoquemon = responsoinfopoquemon["types"];
+
+contenedorpoquemon.innerHTML = "<div class=\"imagen-pokemon\"><img class=\"img-rounded\" width=200 src=\""+urlimagenpoquemon+"\"></div><br>";
+contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>ID: <\/b>"+idpokemonapintar+"</span><br>";
+contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>Nombre: <\/b>"+nombrepoquemon+"</span><br>";
+contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>Altura: <\/b>"+alturapoquemon+"</span><br>";
+contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>Peso: <\/b>"+pesopoquemon+"</span><br>";
+
 let tipopoquemon = "";
 let htmltipospokemon = "<div class=\"list-group-item\"><b>Tipo:<\/b><br><br>";
 let classtipopokemon= "";
 
 for(i=0;i<tipospoquemon.length;i++){
+
   tipopoquemon = tipospoquemon[i]["type"]["name"];
   if(tipopoquemon==="fire"){
     classtipopokemon = "tipopokemonfuego";
@@ -36,11 +46,7 @@ for(i=0;i<tipospoquemon.length;i++){
 
 htmltipospokemon += "</div>";
 
-contenedorpoquemon.innerHTML = "<div class=\"imagen-pokemon\"><img class=\"img-rounded\" width=200 src=\""+urlimagenpoquemon+"\"></div><br>";
-contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>ID: <\/b>"+idpokemonapintar+"</span><br>";
-contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>Nombre: <\/b>"+nombrepoquemon+"</span><br>";
-contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>Altura: <\/b>"+alturapoquemon+"</span><br>";
-contenedorpoquemon.innerHTML = contenedorpoquemon.innerHTML+"<span class=\"list-group-item\"><b>Peso: <\/b>"+pesopoquemon+"</span><br>";
+
 contenedorpoquemon.innerHTML += htmltipospokemon;
 }
 
@@ -52,12 +58,20 @@ async function obtenerinfopoquemon(idpoquemon){
 
 function pintarpokemonsiguiente(){
   idpokemonapintar++;
+  document.querySelector('#idbuscadorpokemon').value = "";
   pintarpokemon();
 }
 
 function pintarpokemonanterior(){
   if(idpokemonapintar>=2)
     idpokemonapintar--;
+  document.querySelector('#idbuscadorpokemon').value = "";
+  pintarpokemon();
+}
+
+function buscarpokemon(){
+  let idbuscadorpoquemon = document.querySelector('#idbuscadorpokemon').value;
+  idpokemonapintar = idbuscadorpoquemon;
   pintarpokemon();
 }
 
